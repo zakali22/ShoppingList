d16e0d5302d5ce2e0ed816942e9011ce503b6403
Zakeria Ali <zak.fined@gmail.com>
2018-09-03 16:47:50 +0100

    Change the Redux devtools extension issue

--- clientside/src/store.js
+++ clientside/src/store.js
@@ -6,6 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
+
+console.log(store.getState());
 
 export default store;
